6c6f73ab3c7f6a8c1c2c59dbc3b3c441eba5325d
Raushan <raushankredugaon@gmail.com>
2025-11-21 13:16:16 +0530

    ApiMethodDiploy

---
+++ index.js
@@ -0,0 +1,25 @@
+const exp = require("express");
+const app = exp();
+app.use(exp.json());
+
+let arr= [
+    {
+        "name":"Raushan",
+        "age":19
+    },
+    {
+        "name":"Aditya",
+        "age":20
+    }
+]
+
+
+app.get("/userNew",(userRequest,userresponse)=>{
+    userresponse.json(arr);
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server Running Successfully");
+    
+})
